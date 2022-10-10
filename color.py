d4623d25c786d7682623d952b7ea64c11aa8835c
Mehedi Hasan <41265022+One-Fist-Man@users.noreply.github.com>
2022-10-10 06:55:22 +0600

    Add files via upload

---
+++ color.py
@@ -0,0 +1,55 @@
+from tkinter import Grid
+
+
+GRID_COLOR = '#000000'
+EMPTY_CELL_COLOR = "#6a0dad"
+SCORE_LABEL_FONT =("Verdana",24)
+SCORE_FONT = ("Helvetica",36,"bold")
+GAME_OVER_FONT = ("Helvetica",48,"bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
+
+CELL_COLORS ={
+    2 : "#fcefe6",
+    4 : "#f2e8cb",
+    8 : "#f5b682",
+   16 : "#ff775c",
+   32 : "#e64c2e",
+   64 : "#ede291",
+  128 : "#fce130",
+  256 : "#ffdb4a",
+  512 : "#f0b922",
+ 1024 : "#ffcc00",
+ 2048 : "#fad74d"
+    
+}
+
+CELL_NUMBER_COLORS = {
+    2 : "#695c57",
+    4 : "#695c57",
+    8 : "#ffffff",
+    16 : "#ffffff",
+    32 : "#ffffff",
+    64 : "#ffffff",
+    128 : "#ffffff",
+    256 : "#ffffff",
+    512 : "#ffffff",
+    1024 : "#ffffff",
+    2048 : "#ffffff"
+}
+
+CELL_NUMBER_FONTS ={
+    2 : ("Helvetica",55,"bold"),
+    4 : ("Helvetica",55,"bold"),
+    8 : ("Helvetica",55,"bold"),
+   16 : ("Helvetica",50,"bold"),
+   32 : ("Helvetica",50,"bold"),
+   64 : ("Helvetica",50,"bold"),
+  128 : ("Helvetica",45,"bold"),
+  256 : ("Helvetica",45,"bold"),
+  512 : ("Helvetica",45,"bold"),
+ 1024 : ("Helvetica",40,"bold"),
+ 2048 : ("Helvetica",40,"bold")
+    
+}
